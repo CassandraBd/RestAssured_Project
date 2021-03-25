b39bbbd88690e648efbbc63ce129a92101aae8f3
CassandraBd <bodencassandra@gmail.com>
2021-03-25 09:28:47 -0500

    New project

---
+++ src/test/java/day1/Junit5_intro.java
@@ -0,0 +1,17 @@
+package day1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Junit5_intro {
+
+    @Test
+    public void test(){
+        System.out.println("Learning JUnit5");
+
+        assertEquals(1,2);
+
+    }
+
+}
