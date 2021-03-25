27e3bae4972aba5d62630d8b072c828d5b2924d2
CassandraBd <bodencassandra@gmail.com>
2021-03-25 10:22:13 -0500

    learning tests

--- src/test/java/day1/Junit5_intro.java
+++ src/test/java/day1/Junit5_intro.java
@@ -1,16 +1,28 @@
 package day1;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+// setting display name of the test class in test result using @DisplayName
+@DisplayName("Day 1 of JUnit5 Test")
 public class Junit5_intro {
 
+    @DisplayName("Testing numbers")
     @Test
     public void test(){
         System.out.println("Learning JUnit5");
 
-        assertEquals(1,2);
+        assertEquals(1,1);
+        assertEquals(1,2, "Something is wrong!!");
+
+    }
+
+    @DisplayName("Testing name starting with A")
+    @Test
+    public void testName(){// add one more test to assert your name first character start with A
+       String name = "Adnan";
+     //   assertEquals('A', name.charAt(0));
+        assertTrue(name.startsWith("A"));
 
     }
 
---
+++ src/test/java/day1/TestLifecycleAnnotations.java
@@ -0,0 +1,42 @@
+package day1;
+
+import org.junit.jupiter.api.*;
+
+// Test Lifecycle annotations
+//@BeforeAll @AfterAll @BeforeEach @AfterEach
+@DisplayName("Learning Test Lifecycle Annotations")
+public class TestLifecycleAnnotations {
+
+    @BeforeAll
+    public static void init(){
+        System.out.println("Before all is running");
+    }
+
+    @AfterAll
+    public static void close(){
+        System.out.println("After all is running");
+    }
+
+    @BeforeEach
+    public void initEach(){
+        System.out.println("Before each is running");
+    }
+
+    @AfterEach
+    public void tearDownEach(){
+        System.out.println("After each is running");
+    }
+
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running");
+    }
+
+}
