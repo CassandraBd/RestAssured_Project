67834f6bdaa62f666c209620e1833e09274af482
CassandraBd <bodencassandra@gmail.com>
2021-03-30 16:00:17 -0500

    day 4 final

--- src/test/java/day4/SpartanRandomPOST_Test.java
+++ src/test/java/day4/SpartanRandomPOST_Test.java
@@ -32,9 +32,9 @@ public class SpartanRandomPOST_Test extends SpartanNoAuthBaseTest {
                 .log().body()
                 .contentType(ContentType.JSON)
                 .body(randomRequestBodyMap).
-                when()
+        when()
                 .post("/spartans").
-                then()
+        then()
                 .log().all()
                 .statusCode(is(201))
                 .body("data.name", is(randomRequestBodyMap.get("name")))
@@ -51,14 +51,95 @@ public class SpartanRandomPOST_Test extends SpartanNoAuthBaseTest {
                 .log().body()
                 .contentType(ContentType.JSON)
                 .body( randomPOJO ).
-                when()
+        when()
                 .post("/spartans").
-                then()
+        then()
                 .log().all()
                 .body("data.name" ,  is( randomPOJO.getName() ) )
                 .body("data.gender" ,  is( randomPOJO.getGender() ) )
                 .body("data.phone" ,  is( randomPOJO.getPhone() ) );
     }
 
+    @DisplayName("POST/spartans and send GET/spartans/{id} to verify")
+    @Test
+    public void testAddOneDataThenGetToVerify(){
+       
+        // send post request, save the id that generated
+        // check status code is 201
+        // send get request using the id you saved from above
+        // check status code is 200 and body match for exactly what you send
 
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+        Response response = given()
+                                    .log().all()
+                                    .contentType(ContentType.JSON)
+                                    .body(randomPOJO).
+                when()
+                                    .post("/spartans").prettyPeek();
+        int newId = response.path("data.id");
+      //  int newId = response.jsonPath().getInt("data.id"); second way to do it
+        System.out.println("newId = " + newId);
+        assertThat(response.statusCode(), is(201));
+
+        given()
+                .log().uri()
+                .pathParam("id", newId).
+        when()
+                .get("/spartans/{id}").
+        then()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(newId))
+                .body("name", is(randomPOJO.getName()))
+                .body("gender", is(randomPOJO.getGender()))
+                .body("phone", is(randomPOJO.getPhone()));
+    }
+
+    @DisplayName("POST/spartans and send GET/spartans/{id} to verify 2")
+    @Test
+    public void testAddOneDataThenGetToVerifyInTheChain(){
+       
+        // send post request verify 202 and then extract the id in the same method chain
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+        int newId = given()
+                            .log().body()
+                            .contentType(ContentType.JSON)
+                            .body(randomPOJO).
+                 when()
+                            .post("/spartans").
+                 then()
+                            .log().body()
+                            .statusCode(201)
+                         //  extract method will enable you to extract data after verification
+                            .extract()
+                            .path("data.id");
+                      //    .jsonPath().getInt("data.id"); second way to extract
+
+
+        System.out.println("newId = " + newId);
+    }
+
+    @DisplayName("POST/spartans and send GET/spartans/{id} to verify 3")
+    @Test
+    public void testAddOneDataThenExtractHeader(){
+       // check status code 201 and extract location header
+        Spartan randomPOJO = SpartanUtil.getRandomSpartanPOJO();
+        String locationHeader = given()
+                                        .log().body()
+                                        .contentType(ContentType.JSON)
+                                        .body(randomPOJO).
+                        when()
+                                        .post("/spartans").
+                        then()
+                                        .statusCode(201)
+                                        .extract().header("location");
+
+        System.out.println("locationHeader = " + locationHeader);
+
+        // sending Get request using above url we extracted
+        get(locationHeader).prettyPeek();
+
+                
+    }
+    
 }
